5073c26111d45db89c533aa5f60b97faf00cd6bb
fanrich <fan@richffan.onmicrosoft.com>
2025-06-08 10:14:19 +0800

    0608

--- source/static/js/custom.js
+++ source/static/js/custom.js
@@ -1,9 +1,3 @@
-// --- 其他配置 (根据需要调整) ---
-var sparkLite_postSelector = "#post > article"; // 文章内容容器的选择器，例如 #article-container, .post-content
-var sparkLite_wordLimit = 1000;             // 提交给 API 的最大字数限制
-var sparkLite_typingAnimate = true;         // 是否启用打字机效果
-
-
 // 那年今日
 (function() {
     async function fetchHistoryData() {
